a2e05bee1c26ab1afe6ddcded926540cf5e0aa43
Szymon Rut <rut.szymon@gmail.com>
2012-06-15 12:14:21 +0200

    Facrories

---
+++ test/factories.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :article do
+    title "Sample"
+    body "text"
+    summary "extra"
+  end
+end
--- test/unit/article_test.rb
+++ test/unit/article_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @note = FactoryGirl.build(:article)
+  end
+
+  test "it should save valid object" do
+    assert @note.save
+  end
+
+  test "it should save without body" do
+    @note.body = ""
+    assert @note.save
+  end
+
+  test "it shouldn't save article without title" do
+    @note.title = nil
+    assert @note.invalid?
+  end
+
+  test "it shouldn't save article without summary" do
+    @note.summary = nil
+    assert @note.invalid?
+  end
 end
