88b51e320e461f15f74fe941b4456e68837bb515
Szymon Rut <rut.szymon@gmail.com>
2012-06-18 15:05:26 +0200

    Add Comment

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '3.2.6'
 
 gem 'sqlite3'
 gem 'activeadmin'
+gem 'devise', '1.1.rc0'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Blog::Application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
 
   resources :pages, only: [:show]
-  resources :articles, only: [:index, :show]
+  resources :articles, only: [:index, :show] do
+    resources :comments, only: [:create]
+  end
 end
