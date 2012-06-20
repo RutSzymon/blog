2c0f3ae0d946cb97d4cece08f116e02ffa4dc6a8
Szymon Rut <rut.szymon@gmail.com>
2012-06-20 12:29:11 +0200

    Counter Cache

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,8 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def top
+    @articles = Article.order(params[:id])
+  end
 end
---
+++ db/migrate/20120620095435_add_countercache_to_article.rb
@@ -0,0 +1,6 @@
+class AddCountercacheToArticle < ActiveRecord::Migration
+  def change
+    add_column :articles, :comments_count, :integer, null: false, default: 0
+    Article.find_each {|art| art.update_attribute(:comments_count, art.comments.size)}
+  end
+end
