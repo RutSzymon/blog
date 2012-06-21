13f150c9b681497ad07b6442a542f10bbde59cf1
Szymon Rut <rut.szymon@gmail.com>
2012-06-21 10:03:52 +0200

    remove article_id from categories and add category_id to articles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120620123228) do
+ActiveRecord::Schema.define(:version => 20120621075928) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -53,11 +53,11 @@ ActiveRecord::Schema.define(:version => 20120620123228) do
     t.datetime "created_at",                    :null => false
     t.datetime "updated_at",                    :null => false
     t.integer  "comments_count", :default => 0, :null => false
+    t.integer  "category_id"
   end
 
   create_table "categories", :force => true do |t|
     t.string   "title"
-    t.integer  "article_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
