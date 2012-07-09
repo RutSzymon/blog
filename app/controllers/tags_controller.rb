0f92e48366aa923c901599044238653281d79c94
Szymon Rut <rut.szymon@gmail.com>
2012-07-09 10:37:41 +0200

    Tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,7 +2,9 @@ class TagsController < ApplicationController
   def index
     @tags = Tag.all
   end
+
   def show
     @tag = Tag.find(params[:id])
+    @articles = @tag.articles.page params[:page]
   end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,8 +1,10 @@
 class Tag < ActiveRecord::Base
   has_and_belongs_to_many :articles
 
-  attr_accessible :title
+  attr_accessible :title, as: :admin  
 
   validates :title, presence: true
+  
+  paginates_per 10
   
 end
