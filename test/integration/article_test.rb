6fddb39df25ddf0c883279176fa62f76437afc92
Szymon Rut <rut.szymon@gmail.com>
2012-07-10 16:06:16 +0200

    Tests

--- test/integration/article_test.rb
+++ test/integration/article_test.rb
@@ -1,3 +1,4 @@
+#encoding: utf-8
 require 'test_helper'
 
 class ArticleTest < ActionDispatch::IntegrationTest
@@ -7,8 +8,10 @@ class ArticleTest < ActionDispatch::IntegrationTest
   end
 
 
-  test "it should have summary" do
+  test "it should have title and summary" do
     visit root_path
+    assert page.has_content?("Tytul 1")
+    assert page.has_content?("Tytul 1")
     assert page.has_content?("wstep 1")
     assert page.has_content?("wstep 2")
   end
@@ -19,4 +22,16 @@ class ArticleTest < ActionDispatch::IntegrationTest
     assert page.has_no_content?("rozszerzenie 2")
   end
 
+  test "it should click link and have body" do
+    visit root_path
+    click_link("zobacz artykuł")
+    assert page.has_content?("rozszerzenie 1")
+  end
+
+  test "it should paginate pages" do
+    visit root_path
+    20.times{|i| FactoryGirl.create(:article, title: "Tytul #{i+2}", summary: "wstep #{i+2}", body: "rozszerzenie #{i+2}") }
+    find("span.last a").click
+    assert page.has_content?("wstep")
+  end
 end
