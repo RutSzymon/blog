d8fa63bbe1a8e8778c06323f09ef4fae4ecbe22b
Szymon Rut <rut.szymon@gmail.com>
2012-07-11 11:18:13 +0200

    Comment integration tests

---
+++ test/integration/comment_test.rb
@@ -0,0 +1,43 @@
+#encoding: utf-8
+require 'test_helper'
+
+class CommentTest < ActionDispatch::IntegrationTest
+   
+  setup do
+     FactoryGirl.create(:comment) 
+  end
+
+  test "article should have a comment" do
+    visit root_path
+    click_link("zobacz artykuł")
+    assert page.has_content?("text")
+  end
+
+  test "it should show a form for logged in user" do
+    visit root_path
+    click_link("Sign in")
+    fill_in("Email", :with => "factory@factory.com")
+    fill_in("Password", :with => "factory")
+    click_button("Sign in")
+    click_link("zobacz artykuł")
+    assert page.has_selector?("#new_comment")
+  end
+
+  test "it shouldn't show a form for anonymous user" do
+    visit root_path
+    click_link("zobacz artykuł")
+    assert page.has_no_selector?("#new_comment")
+  end
+
+  test "it should save a comment" do
+    visit root_path
+    click_link("Sign in")
+    fill_in("Email", :with => "factory@factory.com")
+    fill_in("Password", :with => "factory")
+    click_button("Sign in")
+    click_link("zobacz artykuł")
+    fill_in("comment_body", :with => "Nowy")
+    click_button("Create Comment")
+    assert page.has_content?("Nowy")
+  end
+end
