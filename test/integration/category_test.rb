361b407ea5b7fa2f2df1e984c085f19c29062856
Szymon Rut <rut.szymon@gmail.com>
2012-07-11 11:40:55 +0200

    Category integration tests

---
+++ test/integration/category_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class CategoryTest < ActionDispatch::IntegrationTest
+  
+  setup do
+    FactoryGirl.create(:category)
+  end
+
+  test "it should have title" do
+    visit categories_path
+    assert page.has_content?("Nazwa")
+  end
+
+  test "it should have article" do
+    @category = FactoryGirl.create(:category, title: "Kategoria")
+    FactoryGirl.create(:article, category: @category)
+    visit categories_path
+    click_link("Kategoria")
+    assert page.has_content?("Tytul")
+  end
+
+end
