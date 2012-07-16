1e537b793340e32917314a3490abe803f5feb89a
Szymon Rut <rut.szymon@gmail.com>
2012-07-16 15:51:32 +0200

    Article integration tests

--- test/integration/article_test.rb
+++ test/integration/article_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 
 class ArticleTest < ActionDispatch::IntegrationTest
   setup do
+    @user = FactoryGirl.create(:user, email: "factory@factory.com")
     FactoryGirl.create(:article, title: "Tytul 1", summary: "wstep 1", body: "rozszerzenie 1")
     FactoryGirl.create(:article, title: "Tytul 2", summary: "wstep 2", body: "rozszerzenie 2")
   end
@@ -33,5 +34,31 @@ class ArticleTest < ActionDispatch::IntegrationTest
     visit root_path
     find(".last").click
     assert page.has_content?("wstep")
+  end
+
+  test "add article should show a form" do 
+    visit root_path
+    click_link("Sign in")
+    fill_in("Email", :with => "factory@factory.com")
+    fill_in("Password", :with => "factory")
+    click_button("Sign in")
+    click_link("Add article")
+    assert page.has_button?("Create Article")
+  end
+
+  test "add article should add new article" do 
+    visit root_path
+    click_link("Sign in")
+    fill_in("Email", :with => "factory@factory.com")
+    fill_in("Password", :with => "factory")
+    click_button("Sign in")
+    click_link("Add article")
+    fill_in("Title", :with => "Tytulnew")
+    fill_in("Summary", :with => "Summarynew")
+    fill_in("Body", :with => "Bodynew")
+    select("Nazwa", :from => "Category")
+    fill_in("Tag list", :with => "Tagnew")
+    click_button("Create Article")
+    assert page.has_button?("Create Comment")
   end
 end
